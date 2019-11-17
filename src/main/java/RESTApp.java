20c5b3a5353e5367322d4b170b536ad955e326e2
Samuel <samuelh@stud.ntnu.no>
2019-11-17 17:00:49 +0100

    Restructuring

--- src/main/java/RESTApp.java
+++ src/main/java/RESTApp.java
@@ -1,150 +1,17 @@
-import org.json.JSONObject;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
 
 public class RESTApp {
 
-    private String BASE_URL; // Base URL (address) of the server
+    //Host and port
+    private static String host = "datakomm.work";
+    private static int port = 80;
 
-    public RESTApp(String host, int port){
-        BASE_URL = "http://" + host + ":" + port + "/";
+    //User email and phone number
+    private static String email = "samuelh@stud.ntnu.no";
+    private static String phone = "47224458";
 
-        postAuthorisation("samuelh@stud.ntnu.no", 47224458);
-        getTask();
-
+    public static void main(String[] args) {
+        //Authorize
+        POST post = new POST(host, port);
+        post.postAuthorisation(email,phone);
     }
-
-    /**
-     * Send an HTTP GET to a specific path on the web server
-     */
-    public void getTask() {
-
-        //Task Number
-        String taskNumber = "1";
-
-        //Session ID
-        String sessionId = "21440320";
-
-        String path;
-        path = "dkrest/gettask/" + taskNumber + "?sessionId=" + sessionId;
-        sendGet(path);
-    }
-
-    /**
-     * Send HTTP GET
-     *
-     * @param path     Relative path in the API.
-     */
-    private void sendGet(String path) {
-        try {
-            String url = BASE_URL + path;
-            URL urlObj = new URL(url);
-            System.out.println("Sending HTTP GET to " + url);
-            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
-
-            con.setRequestMethod("GET");
-
-            int responseCode = con.getResponseCode();
-            if (responseCode == 200) {
-                System.out.println("Server reached");
-                // Response was OK, read the body (data)
-                InputStream stream = con.getInputStream();
-                String responseBody = convertStreamToString(stream);
-                stream.close();
-                System.out.println("Response from the server:");
-                System.out.println(responseBody);
-            } else {
-                String responseDescription = con.getResponseMessage();
-                System.out.println("Request failed, response code: " + responseCode + " (" + responseDescription + ")");
-            }
-        } catch (ProtocolException e) {
-            System.out.println("Protocol not supported by the server");
-        } catch (IOException e) {
-            System.out.println("Something went wrong: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Read the whole content from an InputStream, return it as a string
-     * @param is Inputstream to read the body from
-     * @return The whole body as a string
-     */
-    private String convertStreamToString(InputStream is) {
-        BufferedReader in = new BufferedReader(new InputStreamReader(is));
-        StringBuilder response = new StringBuilder();
-        try {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-                response.append('\n');
-            }
-        } catch (IOException ex) {
-            System.out.println("Could not read the data from HTTP response: " + ex.getMessage());
-        }
-        return response.toString();
-    }
-
-    /**
-     * Posts authorization to web server
-     * @param email the email adress of the user
-     * @param phoneNr the phone number of the user
-     */
-    public void postAuthorisation(String email, int phoneNr) {
-        JSONObject json = new JSONObject();
-        json.put("email", email);
-        json.put("phone", phoneNr);
-        System.out.println("Posting authorisation data to server");
-        System.out.println(json.toString());
-        sendPost("dkrest/auth", json);
-    }
-
-    /**
-     * Send HTTP POST
-     *
-     * @param path     Relative path in the API.
-     * @param jsonData The data in JSON format that will be posted to the server
-     */
-    private void sendPost(String path, JSONObject jsonData) {
-        try {
-            String url = BASE_URL + path;
-            URL urlObj = new URL(url);
-            System.out.println("Sending HTTP POST to " + url);
-            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
-
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setDoOutput(true);
-
-            OutputStream os = con.getOutputStream();
-            os.write(jsonData.toString().getBytes());
-            os.flush();
-
-            int responseCode = con.getResponseCode();
-            if (responseCode == 200) {
-                System.out.println("Server reached");
-
-                // Response was OK, read the body (data)
-                InputStream stream = con.getInputStream();
-                String responseBody = convertStreamToString(stream);
-                stream.close();
-                System.out.println("Response from the server:");
-                System.out.println(responseBody);
-            } else {
-                String responseDescription = con.getResponseMessage();
-                System.out.println("Request failed, response code: " + responseCode + " (" + responseDescription + ")");
-            }
-        } catch (ProtocolException e) {
-            System.out.println("Protocol nto supported by the server");
-        } catch (IOException e) {
-            System.out.println("Something went wrong: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-
 }
-
--- src/main/java/main.java
+++
@@ -1,5 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        RESTApp restApp = new RESTApp("datakomm.work", 80);
-    }
-}
